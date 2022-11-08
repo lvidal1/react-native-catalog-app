9a634ede5a32e0fd8dea38decc8c806e3ed8db24
lvidal1 <lvidal910@gmail.com>
2022-11-08 00:30:35 -0500

    Add home flat list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { StyleSheet, View, Text } from "react-native";
+import React, { useEffect, useState } from 'react'
+import {
+    ActivityIndicator,
+    FlatList,
+    Image,
+    StyleSheet,
+    Text,
+    TouchableWithoutFeedback,
+    View,
+} from "react-native";
 
-export default function Home() {
+const API_KEY = '&apikey=d36af94423444744b87b93f244cc48e0'
+
+export default function Home({ navigation }) {
+    const [loading, setLoading] = useState(true);
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+
+        fetch(`http://newsapi.org/v2/everything?q=twitter${API_KEY}`)
+            .then((response) => response.json())
+            .then((json) => setNews(json.articles))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false))
+    }, []);
+
+    const storyItem = ({ item }) => {
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => navigation.navigate('NewsDetail', { url: item.url })}
+            >
+                <View style={styles.listings}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Image
+                        style={styles.thumbnail}
+                        source={{ uri: item.urlToImage }}
+                    />
+                    <Text style={styles.blurb}>{item.description}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
+
     return (
         <View style={styles.container}>
-            <Text>This will be the Home Page</Text>
+            {loading ? <ActivityIndicator /> : (<FlatList
+                data={news}
+                renderItem={storyItem}
+                keyExtractor={(item) => item.url} />)}
         </View>
     )
 }
@@ -14,6 +56,27 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        width: '100%',
+        padding: 20
+    },
+    thumbnail: {
+        height: 100,
+        width: '98%'
+    },
+    listings: {
+        paddingTop: 15,
+        paddingBottom: 25,
+        borderBottomColor: '#bbb',
+        borderBottomWidth: 1
+    },
+    title: {
+        paddingBottom: 10,
+        fontFamily: 'OpenSans'
+    },
+    blurb: {
+        fontFamily: 'OpenSans',
+        fontStyle: 'italic'
     }
+
 })
